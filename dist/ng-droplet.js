02040519f9047e7d4cd68faa15a251092a4014d0
Wildhoney <adam.timberlake@gmail.com>
2014-10-13 21:39:41 +0100

    Build

--- dist/ng-droplet.js
+++ dist/ng-droplet.js
@@ -3,6 +3,60 @@
     "use strict";
 
     // The truest wisdom is a resolute determination...
-    $angular.module('ngDroplet', []);
+    var module = $angular.module('ngDroplet', []);
+
+    module.directive('droplet', function DropletDirective() {
+
+        return {
+
+            /**
+             * @property restrict
+             * @type {String}
+             */
+            restrict: 'EA',
+
+            /**
+             * @property scope
+             * @type {Boolean}
+             */
+            scope: {
+                moduleInterface: '=interface'
+            },
+
+            /**
+             * @property transclude
+             * @type {Boolean}
+             */
+            transclude: true,
+
+            /**
+             * @method controller
+             * @param $scope {Object}
+             * @return {void}
+             */
+            controller: ['$scope', function DropletController($scope) {
+
+                /**
+                 * @property files
+                 * @type {Array}
+                 */
+                $scope.files = [];
+
+            }],
+
+            /**
+             * @method link
+             * @param scope {Object}
+             * @return {void}
+             */
+            link: function link(scope, element, attributes, transclude) {
+
+                transclude();
+
+            }
+
+        }
+
+    });
 
 })(window.angular);
