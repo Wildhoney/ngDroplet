76dfbdd5aa65ed02d6fc578850ac2600243a5610
Wildhoney <adam.timberlake@gmail.com>
2014-10-18 12:10:51 +0100

    Added success msg

--- example/js/app/Default.js
+++ example/js/app/Default.js
@@ -1,13 +1,25 @@
 (function($angular) {
 
     // And... and... who other than you knows how to count the stars?
-    $angular.module('dropletApp', ['ngDroplet']).controller('IndexController', function IndexController($scope) {
+    $angular.module('dropletApp', ['ngDroplet']).controller('IndexController', function IndexController($scope, $timeout) {
 
         /**
          * @property interface
          * @type {Object}
          */
         $scope.interface = {};
+
+        /**
+         * @property uploadCount
+         * @type {Number}
+         */
+        $scope.uploadCount = 0;
+
+        /**
+         * @property success
+         * @type {Number}
+         */
+        $scope.success = 0;
 
         // Listen for when the interface has been configured.
         $scope.$on('$dropletReady', function whenDropletReady() {
@@ -21,7 +33,13 @@
         // Listen for when the files have been successfully uploaded.
         $scope.$on('$dropletSuccess', function onDropletSuccess(event, response, files) {
 
+            $scope.uploadCount = files.length;
+            $scope.success     = true;
             console.log(response, files);
+
+            $timeout(function timeout() {
+                $scope.success = false;
+            }, 5000);
 
         });
 
