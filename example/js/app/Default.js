12b98d4b7756ad784eea402f7dd42abfab4208bf
Wildhoney <adam.timberlake@gmail.com>
2014-10-20 17:42:06 +0100

    Updated styles in example

--- example/js/app/Default.js
+++ example/js/app/Default.js
@@ -61,6 +61,62 @@
 
         });
 
+    }).directive('progressbar', function ProgressbarDirective() {
+
+        return {
+
+            /**
+             * @property restrict
+             * @type {String}
+             */
+            restrict: 'A',
+
+            /**
+             * @property scope
+             * @type {Object}
+             */
+            scope: {
+                model: '=ngModel'
+            },
+
+            /**
+             * @property ngModel
+             * @type {String}
+             */
+            require: 'ngModel',
+
+            /**
+             * @method link
+             * @param scope {Object}
+             * @param element {Object}
+             * @return {void}
+             */
+            link: function link(scope, element) {
+
+                var progressBar = new ProgressBar.Path(element[0], {
+                    strokeWidth: 2
+                });
+
+                scope.$watch('model', function() {
+
+                    progressBar.animate(scope.model / 100, {
+                        duration: 1000
+                    });
+
+                });
+
+                scope.$on('$dropletSuccess', function onSuccess() {
+                    progressBar.animate(0);
+                });
+
+                scope.$on('$dropletError', function onSuccess() {
+                    progressBar.animate(0);
+                });
+
+            }
+
+        }
+
     });
 
 })(window.angular);
